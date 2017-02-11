b0789cf45b4ed307f5de13731d7f683c666def3c
cavemansspa <cavemansspa@gmail.com>
2017-02-11 10:02:40 -0500

    Move navigation block to Page.js

--- mithril/views/Main.js
+++ mithril/views/Main.js
@@ -25,52 +25,6 @@ CavemansSPA = (function () {
         model: {},
         store: {},
 
-        navigation: {
-            history: [],
-            direction: 'next',
-            pageInClass: function () {
-                return (this.direction === 'next' ? 'slideInRight' : 'slideInLeft')
-            },
-            pageOutClass: function () {
-                return (this.direction === 'next' ? 'slideOutLeft' : 'slideOutRight')
-            },
-            lastPage: null,
-            removeClasses: function (vnode, className) {
-                return function (e) {
-                    console.log('CavemansSPA.view.Page::removeClasses()', vnode);
-                    vnode.dom.classList.remove('cavemansspa-page-new', 'animated', className);
-                    e.target.removeEventListener(e.type, arguments.callee);
-                }
-            },
-            doTransition: function (vnode, args) {
-                var lastPageHistory = _.nth(CavemansSPA.navigation.history, -2);
-                //debugger;
-                if (lastPageHistory === args.key) {
-                    CavemansSPA.navigation.history.pop();
-                    CavemansSPA.navigation.direction = 'prev';
-                } else {
-                    CavemansSPA.navigation.direction = 'next';
-                    CavemansSPA.navigation.history.push(args.key);
-                }
-                vnode.dom.addEventListener('animationend', CavemansSPA.navigation.removeClasses(vnode, CavemansSPA.navigation.pageInClass()));
-
-                console.log('direction: ', CavemansSPA.navigation.direction,
-                    ', pageInClass:', CavemansSPA.navigation.pageInClass(),
-                    ', pageOutClass: ', CavemansSPA.navigation.pageOutClass()
-                );
-
-                vnode.dom.classList.add('cavemansspa-page-new', 'animated', CavemansSPA.navigation.pageInClass());
-
-                var theLastPage = CavemansSPA.navigation.lastPage;
-                theLastPage.vnode.dom.classList.add('cavemansspa-page-last', 'animated', CavemansSPA.navigation.pageOutClass());
-                theLastPage.vnode.dom.addEventListener('animationend', function (el) {
-                    console.log('in onbeforeremove addEventListener', el);
-                    console.log(theLastPage);
-                    theLastPage.whenDone();
-                });
-            }
-        },
-
         onReady: function () {
             m.route(document.querySelector('body .app-body'), '/login', {
                 '/login': CavemansSPA.view.Page(CavemansSPA.view.Login, {
--- mithril/views/Page.js
+++ mithril/views/Page.js
@@ -1,8 +1,54 @@
-CavemansSPA.view.Page = function(component, args) {
+CavemansSPA.navigation = {
+    history: [],
+    direction: 'next',
+    pageInClass: function () {
+        return (this.direction === 'next' ? 'slideInRight' : 'slideInLeft')
+    },
+    pageOutClass: function () {
+        return (this.direction === 'next' ? 'slideOutLeft' : 'slideOutRight')
+    },
+    lastPage: null,
+    removeClasses: function (vnode, className) {
+        return function (e) {
+            console.log('CavemansSPA.view.Page::removeClasses()', vnode);
+            vnode.dom.classList.remove('cavemansspa-page-new', 'animated', className);
+            e.target.removeEventListener(e.type, arguments.callee);
+        }
+    },
+    doTransition: function (vnode, args) {
+        var lastPageHistory = _.nth(CavemansSPA.navigation.history, -2);
+        //debugger;
+        if (lastPageHistory === args.key) {
+            CavemansSPA.navigation.history.pop();
+            CavemansSPA.navigation.direction = 'prev';
+        } else {
+            CavemansSPA.navigation.direction = 'next';
+            CavemansSPA.navigation.history.push(args.key);
+        }
+        vnode.dom.addEventListener('animationend', CavemansSPA.navigation.removeClasses(vnode, CavemansSPA.navigation.pageInClass()));
+
+        console.log('direction: ', CavemansSPA.navigation.direction,
+            ', pageInClass:', CavemansSPA.navigation.pageInClass(),
+            ', pageOutClass: ', CavemansSPA.navigation.pageOutClass()
+        );
+
+        vnode.dom.classList.add('cavemansspa-page-new', 'animated', CavemansSPA.navigation.pageInClass());
+
+        var theLastPage = CavemansSPA.navigation.lastPage;
+        theLastPage.vnode.dom.classList.add('cavemansspa-page-last', 'animated', CavemansSPA.navigation.pageOutClass());
+        theLastPage.vnode.dom.addEventListener('animationend', function (el) {
+            console.log('in onbeforeremove addEventListener', el);
+            console.log(theLastPage);
+            theLastPage.whenDone();
+        });
+    }
+};
+
+CavemansSPA.view.Page = function (component, args) {
     console.log('CavemansSPA.view.Page', component, args);
     return {
 
-        oncreate: function(vnode) {
+        oncreate: function (vnode) {
             console.log('CavemansSPA.view.Page::oncreate()', vnode);
 
             if (!CavemansSPA.navigation.lastPage) {
@@ -14,22 +60,21 @@ CavemansSPA.view.Page = function(component, args) {
             CavemansSPA.navigation.doTransition(vnode, args.pageArgs);
         },
 
-        onbeforeremove: function(vnode) {
+        onbeforeremove: function (vnode) {
             console.log('CavemansSPA.view.Page::onbeforeremove()', vnode, done);
 
             CavemansSPA.navigation.lastPage = {
-                //whenDone: done,
                 vnode: vnode,
                 key: args.pageArgs.key
             };
-            var done = new Promise(function(resolve) {
+            var done = new Promise(function (resolve) {
                 console.log('done promise', resolve);
                 CavemansSPA.navigation.lastPage.whenDone = resolve;
-            })
+            });
             return done;
         },
 
-        view: function(vnode) {
+        view: function (vnode) {
             console.log('CavemansSPA.view.Page::view()', vnode, args);
             return m('div.cavemansspa-page', args.pageArgs, [
                 'test',
@@ -37,4 +82,4 @@ CavemansSPA.view.Page = function(component, args) {
             ]);
         }
     }
-}
+};
